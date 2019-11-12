d15fc55beea99fc1dc4e08d1398f6213a475f148
Steve Howell <showell30@yahoo.com>
2019-11-12 12:48:35 -0500

    Initial setup

---
+++ src/Main.elm
@@ -0,0 +1,69 @@
+module Main exposing (main)
+
+import Browser
+
+
+type alias Model =
+    { title : String
+    }
+
+
+type Msg
+    = Never
+
+
+
+-- MODEL / INIT
+
+
+main : Program () Model Msg
+main =
+    Browser.document
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    let
+        model =
+            { title = "simple demo"
+            }
+    in
+    ( model, Cmd.none )
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Browser.Document Msg
+view model =
+    let
+        _ =
+            Debug.log "hello" "hello"
+    in
+    { title = model.title
+    , body = []
+    }
